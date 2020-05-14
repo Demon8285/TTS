c9da04fbb8973897bf9675f2a2609d5327a36230
proto <proto@DESKTOP-VQ24F76>
2020-05-14 20:57:47 +0300

    Добавьте файлы проекта.

---
+++ TTS/Controllers/ProjectController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.V3.Pages.Internal;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TTS.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Tasks(string id)
+        {
+            return View();
+        }
+        public bool TaskDelete(string id)
+        {
+            return true;
+        }
+        public bool ProjectDelete(string id)
+        {
+            return true;
+        }
+        public bool UpdateTaskName (string id, string new_name)
+        {
+            return true;
+        }
+        public bool UpdateTaskTime(string id, double new_time)
+        {
+            return true;
+        }
+        public bool UpdateTaskStatus(string id, bool new_status)
+        {
+            return true;
+        }
+        public string AddTask(string name)
+        {
+            return Guid.NewGuid().ToString();
+        }
+        public IActionResult Add(string name)
+        {
+            return Redirect("https://" + name);
+        }
+    }
+}
---
+++ TTS/wwwroot/js/TaskListScript.js
@@ -0,0 +1,49 @@
+﻿$(".btn-danger").click(function () {
+    var id_t = $(this).attr("id");
+    var tr = $(this).parent().parent();
+    $.get("/Project/TaskDelete", { id: id_t }, function (data) {
+        tr.remove();
+    });
+});
+$(".taskname").on("change", function () {
+    var value = $(this).val().toLowerCase();
+    var id_t = $(this).attr("id");
+    $.get("/Project/UpdateTaskName", { id: id_t, new_name: value }, function (data) {
+        if (data) {
+            alert("Ви змінили назву");
+        }
+    });
+});
+$("#taskinput").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $("tbody tr").filter(function () {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+});
+$(".btn-outline-success").click(function () {
+    var name = $("#taskinput").val();
+    $.get("/Project/AddTask", { name: name }, function (data) {
+        var newrow = '<tr><th><input class="form-control addtask" type="text" value="' + name + '"id="' + data + '"/></th> <th>1</th>'+
+            '<th><input type="checkbox" class="form-check" checked="checked" /></th>'+
+            '<th><a class="btn btn-danger text-white" id="1sdds">Видалить</a></th></tr>'
+        $("tbody").append(newrow);
+    });
+});
+$(".tasktime").on("change", function () {
+    var value = $(this).val().toLowerCase();
+    var id_t = $(this).attr("id");
+    $.get("/Project/UpdateTaskTime", { id: id_t, new_time: value }, function (data) {
+        if (data) {
+            alert("Ви змінили час");
+        }
+    });
+});
+$(".taskstatus").on("change", function (event) {
+    var value = event.target.checked
+    var id_t = $(this).attr("id");
+    $.get("/Project/UpdateTaskStatus", { id: id_t, new_status: value }, function (data) {
+        if (data) {
+            alert("Ви змінили статус");
+        }
+    });
+});
